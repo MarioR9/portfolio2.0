a755101bb2a15c2c530798c02303e719efc02a7a
MarioR9 <mariorodriguezan@gmail.com>
2020-01-22 21:33:55 -0500

    new Components file

---
+++ src/components/navigationBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class NavigationBar extends React.Component{
+    render(){
+  
+        return(
+            <div>
+                <h1>Hellow World</h1>
+            </div>
+        )}
+
+}
