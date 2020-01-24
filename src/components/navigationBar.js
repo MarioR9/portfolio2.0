91691813f36ab627cfd34a5d1c84469e1ac14e40
MarioR9 <mariorodriguezan@gmail.com>
2020-01-24 12:37:49 -0500

    navaigation bar and links added

--- src/components/navigationBar.js
+++ src/components/navigationBar.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav} from 'react-bootstrap';
 
 export default class NavigationBar extends React.Component{
     render(){
         return(
             <div>
-                <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar bg="light" expand="lg" fixed="top">
+                <Navbar.Brand href="/home">Mario Rodriguez</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
+                    <Nav className="mr-auto" ></Nav>
+                    <Nav className="navbar-left">
+                    <Nav.Link href="/about">About</Nav.Link>
+                    <Nav.Link href="/projects">Projects</Nav.Link>
+                    <Nav.Link href="/contact">Contact</Nav.Link>
+                    <Nav.Link href="/resume">Resume</Nav.Link>
+
                     </Nav>
                 </Navbar.Collapse>
                 </Navbar>
