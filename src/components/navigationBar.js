2fff23add1087f03ad96f852865df3bbff174deb
marior9 <mariorodriguezan@gmail.com>
2020-02-04 19:54:00 -0500

    black bk and white text with with icons for navigationbar

--- src/components/navigationBar.js
+++ src/components/navigationBar.js
@@ -6,14 +6,17 @@ export default class NavigationBar extends React.Component{
     render(){
         return(
             <div>
-                <Navbar bg="light" expand="lg" fixed="top">
-                <Navbar.Brand href="/home">Mario Rodriguez</Navbar.Brand>
+                <Navbar bg="dark" expand="lg" variant="dark" fixed="top">
+                <Navbar.Brand href="/home">Mario Rodriguez
+                &nbsp;
+                <SocialIcon network="github" bgColor="#f5eaea" style={{ height: 40, width: 40 }} url="https://github.com/MarioR9"/> 
+                &nbsp;
+                <SocialIcon network="linkedin" bgColor="#f5eaea" style={{ height: 40, width: 40 }} url="https://www.linkedin.com/in/mario-rodriguezan/"/>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto" ></Nav>
                     <Nav className="navbar-left">
-                    <SocialIcon network="github" bgColor="##000000" style={{ height: 40, width: 40 }} url="https://github.com/MarioR9"/> 
-                    <SocialIcon network="linkedin"  style={{ height: 40, width: 40 }} url="https://www.linkedin.com/in/mario-rodriguezan/"/>
                     <Nav.Link href="/about">About</Nav.Link>
                     <Nav.Link href="/projects">Projects</Nav.Link>
                     <Nav.Link href="/contact">Contact</Nav.Link>
