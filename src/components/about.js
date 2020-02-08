2c60388dab0eb892ad021e2899eecb018ebad9fe
marior9 <mariorodriguezan@gmail.com>
2020-02-08 16:41:23 -0500

    about font and size

--- src/components/about.js
+++ src/components/about.js
@@ -1,20 +1,25 @@
 import React from 'react'
-
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default class About extends React.Component{
     render(){
         return(
-            <div> 
-            &nbsp;
+            <div>
+            <Container>
                 <div className='about-Header '>
-                    
-                <h1>About Me</h1>
-                <img className='img2' src="../assets/0.jpg" />
-                &nbsp;
+                <Row>
+                <Col>
+                <div>
+                <h1 >about me</h1>
                 <p className='description'>Full-stack web developer with a passion for problem-solving. With experience in Ruby on Rails, JavaScript, and React I discovered web development while taking computer science classes and I fell in love with it. I bring strong skills in working with people, communication in English and Spanish as well as managing people. I spend my free time playing guitar as well as building small projects and learning about new technologies.</p>
-                
                 </div>
-           
+                </Col>
+                <Col>
+                <img className='img2' src="../assets/0.jpg" />
+                </Col>
+                </Row>
+                </div>
+            </Container>
             </div>
         )
     }
